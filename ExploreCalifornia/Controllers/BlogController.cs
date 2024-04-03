7d25900c5da6ec72ccbe0c055a6f412d23dc397b
diaaeldin.elsherif.ext <diaaeldin.elsherif.ext@kiongroup.com>
2024-04-03 20:18:33 +0200

    render data with strongly typed views

--- ExploreCalifornia/Controllers/BlogController.cs
+++ ExploreCalifornia/Controllers/BlogController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ExploreCalifornia.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ExploreCalifornia.Controllers
 {
@@ -12,11 +13,14 @@ namespace ExploreCalifornia.Controllers
         [Route("blog/{year:min(2000)}/{month:range(1,12)}/{key}")]
         public IActionResult Post(int year, int month, string key)
         {
-            ViewBag.Title = "My blog post";
-            ViewBag.Posted = DateTime.Now;
-            ViewBag.Author = "Diaaeldin Elsherif";
-            ViewBag.Body = "This is a great blog post.";
-            return View();
+            var post = new Post
+            {
+                Title = "My blog post",
+                Posted = DateTime.Now,
+                Author = "Diaaeldin Elsherif",
+                Body = "This is a great blog post."
+            };
+            return View(post);
         }
     }
 }
