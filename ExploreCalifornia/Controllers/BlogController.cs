076ed6feba8fb9cf6cd8b27cd066ac0788959d00
diaaeldin.elsherif.ext <diaaeldin.elsherif.ext@kiongroup.com>
2024-04-03 17:53:36 +0200

    reuse shared html markup with layouts

--- ExploreCalifornia/Controllers/BlogController.cs
+++ ExploreCalifornia/Controllers/BlogController.cs
@@ -6,13 +6,13 @@ namespace ExploreCalifornia.Controllers
     {
         public IActionResult Index()
         {
-            return new ContentResult { Content = "Blog posts" };
+            return View();
         }
 
         [Route("blog/{year:min(2000)}/{month:range(1,12)}/{key}")]
         public IActionResult Post(int year, int month, string key)
         {
-            return new ContentResult { Content = $"Year: {year}; Month: {month}; Key: {key}" };
+            return View();
         }
     }
 }
