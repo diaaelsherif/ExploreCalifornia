1e46e2505ee5118c2a7b342729ea31b1ded0779f
diaaeldin.elsherif.ext <diaaeldin.elsherif.ext@kiongroup.com>
2024-04-03 19:43:51 +0200

    view bag property to pass data from controller to view

--- ExploreCalifornia/Controllers/BlogController.cs
+++ ExploreCalifornia/Controllers/BlogController.cs
@@ -12,6 +12,10 @@ namespace ExploreCalifornia.Controllers
         [Route("blog/{year:min(2000)}/{month:range(1,12)}/{key}")]
         public IActionResult Post(int year, int month, string key)
         {
+            ViewBag.Title = "My blog post";
+            ViewBag.Posted = DateTime.Now;
+            ViewBag.Author = "Diaaeldin Elsherif";
+            ViewBag.Body = "This is a great blog post.";
             return View();
         }
     }
