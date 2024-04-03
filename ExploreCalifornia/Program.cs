cf4810ab721ba34117690417185edd49b72cd46a
diaaeldin.elsherif.ext <diaaeldin.elsherif.ext@kiongroup.com>
2024-04-03 11:00:37 +0200

    use exception handler

--- ExploreCalifornia/Program.cs
+++ ExploreCalifornia/Program.cs
@@ -1,5 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+var configuration = app.Configuration;
+
+app.UseExceptionHandler("/error.html");
+
+if (configuration.GetValue<bool>("EnableDeveloperExceptions"))
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.Use(async (context, next) =>
+{
+    if (context.Request.Path.Value.Contains("invalid"))
+        throw new Exception("ERROR!");
+    
+    await next();
+});
 
 app.UseFileServer();
 app.Run();
