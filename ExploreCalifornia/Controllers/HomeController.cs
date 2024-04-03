26b44ea58855b7922192e1f5baa640689cb518ab
diaaeldin.elsherif.ext <diaaeldin.elsherif.ext@kiongroup.com>
2024-04-03 15:40:29 +0200

    home controller (/home/index)

---
+++ ExploreCalifornia/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExploreCalifornia.Controllers
+{
+    public class HomeController
+    {
+        public string Index()
+        {
+            return "Hello, ASP.NET Core MVC!";
+        }
+    }
+}
