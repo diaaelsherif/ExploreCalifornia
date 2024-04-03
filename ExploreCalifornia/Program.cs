cc6354ccc5192a1c91cf4574192422f086538515
diaaeldin.elsherif.ext <diaaeldin.elsherif.ext@kiongroup.com>
2024-04-03 09:47:46 +0200

    wwwroot static content and use file server

--- ExploreCalifornia/Program.cs
+++ ExploreCalifornia/Program.cs
@@ -1,21 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-//Middleware
-app.Use(async (context, next) =>
-{
-    if (context.Request.Path.Value.StartsWith("/hello"))
-    {
-        await context.Response.WriteAsync("Hello ASP.NET Core!");
-    }
-
-    await next();
-});
-
-//Terminal Middleware
-app.Run(async (context) =>
-{
-    await context.Response.WriteAsync(" How are you?");
-});
-
+app.UseFileServer();
 app.Run();
