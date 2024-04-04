c4ee321ea116b44c769e21606886f76cea6337e6
diaaelsherif <diaa.elsherif@msn.com>
2024-04-04 06:33:14 +0200

    create strongly typed html forms, post form data to controller action, and validate form post data

--- ExploreCalifornia/Controllers/BlogController.cs
+++ ExploreCalifornia/Controllers/BlogController.cs
@@ -3,14 +3,34 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ExploreCalifornia.Controllers
 {
+    [Route("blog")]
     public class BlogController : Controller
     {
+        [Route("")]
         public IActionResult Index()
         {
-            return View();
+            var posts = new[]
+            {
+                new Post
+                {
+                    Title = "My blog post",
+                    Posted = DateTime.Now,
+                    Author = "Diaaeldin Elsherif",
+                    Body = "This is a great blog post.",
+                },
+                new Post
+                {
+                    Title = "My second blog post",
+                    Posted = DateTime.Now,
+                    Author = "Diaaeldin Elsherif",
+                    Body = "This is another great blog post.",
+                },
+            };
+
+            return View(posts);
         }
 
-        [Route("blog/{year:min(2000)}/{month:range(1,12)}/{key}")]
+        [Route("{year:min(2000)}/{month:range(1,12)}/{key}")]
         public IActionResult Post(int year, int month, string key)
         {
             var post = new Post
@@ -21,6 +41,24 @@ namespace ExploreCalifornia.Controllers
                 Body = "This is a great blog post."
             };
             return View(post);
+        }
+
+        [HttpGet, Route("create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost, Route("create")]
+        public IActionResult Create(Post post)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            post.Author = User.Identity.Name;
+            post.Posted = DateTime.Now;
+
+            return View();
         }
     }
 }
